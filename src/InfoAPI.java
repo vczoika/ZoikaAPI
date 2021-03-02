72fa6c85f2bcb1ee4e978d9271b995275707d015
zoika <zoika@pop-os.localdomain>
2021-03-02 20:10:40 -0300

    Initial commit

---
+++ src/InfoAPI.java
@@ -0,0 +1,37 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.*;
+import java.util.Scanner;
+
+public class InfoAPI {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Input your URL to Scrape: ");
+        String url = userInput.nextLine();// Website URL
+        int listSize = 1;
+        System.out.println("Input maximum size for list: ");
+        int maximumListSize = userInput.nextInt();
+        try {
+            PrintWriter JSONWriter = new PrintWriter("scrape.json"); // Creates txt file to print info
+            Document doc = Jsoup.connect(url).get(); // Gets Website info
+            Elements htmlGet = doc.select("div.mtitle"); // Selects the element of the website
+            JSONWriter.println("{\n" + "   " + "\"movie\": [");
+            for (Element movieList:htmlGet) { // Runs while there's movies
+                if (listSize <= (maximumListSize - 1)) {
+                    listSize++;
+                    JSONWriter.print("      " + "{\n" + "         " + "\"movieName\": " + "\"" + movieList.getElementsByTag("a").first().text() + "\"\n" + "      " + "}" + "," + "\n"); // Writes to file
+                    System.out.println(movieList.getElementsByTag("a").first().text()); // Prints to console, check if it's correct!
+                } else if (listSize == maximumListSize) {
+                    listSize++;
+                    JSONWriter.print("      " + "{\n" + "         " + "\"movieName\": " + "\"" + movieList.getElementsByTag("a").first().text() + "\"\n" + "      " + "}" + "\n");
+                    System.out.println(movieList.getElementsByTag("a").first().text());
+                }
+            } JSONWriter.println("   " + "]" + "\n}");
+            JSONWriter.close(); // Ends the writer
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
